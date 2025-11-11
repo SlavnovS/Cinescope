f39c623cbd1c0709ca68dfd1136f138edbbdd945
Alexandr <sashe-slavnov@mail.ru>
2025-11-11 13:48:56 +0300

    check test_delete_user_by_id_common_user

---
+++ entities/user.py
@@ -0,0 +1,16 @@
+from api.api_manager import ApiManager
+
+class User:
+    def __init__(self, email: str, password: str, roles: list, api: ApiManager):
+        self.email = email
+        self.password = password
+        self.roles = roles
+        self.api = api  # Сюда будем передавать экземпляр API Manager для запросов
+
+    @property
+    def creds(self):
+        """Возвращает кортеж (email, password)"""
+        return {"email": self.email,
+                "password": self.password}
+        # return self.email, self.password
+
