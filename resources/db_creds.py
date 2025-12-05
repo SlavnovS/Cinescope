9e3af95c38f9bed36b776c0bcd5f5ec38b9f7ae8
Alexandr <sashe-slavnov@mail.ru>
2025-12-05 18:28:32 +0300

    CI/CD

--- resources/db_creds.py
+++ resources/db_creds.py
@@ -3,6 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("HOST:", os.getenv("DB_MOVIES_HOST"))
+print("PORT:", os.getenv("DB_MOVIES_PORT"))
+print("URL :", os.getenv("DB_MOVIES_URL"))
+
 class MoviesDataBaseCreds:
     DATABASE_NAME = os.getenv('DB_MOVIES_NAME')
     USERNAME = os.getenv('DB_MOVIES_USERNAME')
