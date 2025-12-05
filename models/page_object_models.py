1e6d79c2a8b2e625e802314178ebf0787761cd25
Alexandr <sashe-slavnov@mail.ru>
2025-12-05 17:41:43 +0300

    CI/CD

--- models/page_object_models.py
+++ models/page_object_models.py
@@ -1,7 +1,10 @@
-from playwright.sync_api import Page
+from playwright.sync_api import Page, expect
 import allure
+from abc import ABC, abstractmethod
+from typing import Literal
 
-class PageAction:
+
+class PageAction(ABC):
     def __init__(self, page: Page):
         self.page = page
 
@@ -27,7 +30,7 @@ class PageAction:
         return self.page.locator(locator).text_content()
 
     @allure.step("Ожидание появления или исчезновения элемента: {locator}, state = {state}")
-    def wait_for_element(self, locator: str, state: str = "visible"):
+    def wait_for_element(self, locator: str, state: Literal["attached", "detached", "visible", "hidden"] = "visible"):
         self.page.locator(locator).wait_for(state=state)
 
     @allure.step("Скриншот текущей страиницы")
@@ -40,7 +43,7 @@ class PageAction:
             allure.attach(file.read(), name="Screenshot after redirect", attachment_type=allure.attachment_type.PNG)
 
     @allure.step("Проверка всплывающего сообщения c текстом: {text}")
-    def check_pop_up_element_with_text(self, text: str) -> bool:
+    def check_pop_up_element_with_text(self, text: str) -> None:
         with allure.step("Проверка появления алерта с текстом: '{text}'"):
             notification_locator = self.page.get_by_text(text)
             # Ждем появления элемента
@@ -52,6 +55,14 @@ class PageAction:
             notification_locator.wait_for(state="hidden")
             assert notification_locator.is_visible() == False, "Уведомление не исчезло"
 
+    @abstractmethod
+    def check_enabled_home_button(self, home_button: str) -> None:
+        expect (self.page.locator(home_button)).to_be_enabled()
+
+    @abstractmethod
+    def check_enabled_all_movies_button(self, all_movies_button: str) -> None:
+        expect (self.page.locator(all_movies_button)).to_be_enabled()
+
 
 class BasePage(PageAction): #Базовая логика доспустимая для всех страниц на сайте
     def __init__(self, page: Page):
@@ -59,8 +70,8 @@ class BasePage(PageAction): #Базовая логика доспустимая 
         self.home_url = "https://dev-cinescope.coconutqa.ru/"
 
         # Общие локаторы для всех страниц на сайте
-        self.home_button = "a[href='/' and text()='Cinescope']"
-        self.all_movies_button = "a[href='/movies' and text()='Все фильмы']"
+        self.home_button = "a.text-3xl[href='/']"
+        self.all_movies_button = "a[href='/movies']"
 
     @allure.step("Переход на главную страницу, из шапки сайта")
     def go_to_home_page(self):
@@ -71,6 +82,14 @@ class BasePage(PageAction): #Базовая логика доспустимая 
     def go_to_all_movies(self):
         self.click_element(self.all_movies_button)
         self.wait_redirect_for_url(f"{self.home_url}movies")
+
+    @allure.step("Проверка кликабельности кнопки перехода на главную страницу")
+    def check_enabled_home_button(self):
+        super().check_enabled_home_button(self.home_button)
+
+    @allure.step("Проверка кликабельности кнопки перехода на страницу 'Все фильмы'")
+    def check_enabled_all_movies_button(self):
+        super().check_enabled_all_movies_button(self.all_movies_button)
 
 class CinescopRegisterPage(BasePage):
     def __init__(self, page: Page):
